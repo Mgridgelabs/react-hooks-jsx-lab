d3a2867ce555ca1543806fe18ca9ec4014150fd9
Mgridgelabs <mainaianndungu@gmail.com>
2024-08-01 01:50:44 +0300

    completes about home and navbar jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Ola ola!!!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
+
